4c5f9cd5695e375e2cc2ef9731a31bfc6e9d06c1
CapyyyHub <andreiantonio183@gmail.com>
2024-06-06 04:28:26 +0800

    orionhub.lua

---
+++ orionhub.lua
@@ -0,0 +1,16 @@
+if game.PlaceId == 8737899170 then
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "⚡Vital Hub", HidePremium = false, IntroText = "Vital Hub" SaveConfig = true, ConfigFolder = "OrionTest"})|
+
+local Information = Window:MakeTab({
+	Name = "INFO",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+
+
+
+
+end
+OrionLib:Init()
